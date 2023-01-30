710614f17d68a89d43b340ec41322005daeee4b7
Malavong <1948068@cmontmorency.qc.ca>
2023-01-30 11:38:27 -0500

    module 2 pong

--- pong/src/main/java/pong/PongLocal.java
+++ pong/src/main/java/pong/PongLocal.java
@@ -5,36 +5,32 @@ import ca.ntro.app.backend.BackendRegistrar;
 import ca.ntro.app.frontend.FrontendRegistrarFx;
 import ca.ntro.app.messages.MessageRegistrar;
 import ca.ntro.app.models.ModelRegistrar;
+import pong.frontal.FrontalPong;
 
 public class PongLocal implements NtroClientFx {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		NtroClientFx.launch(args);
-	}
+    public static void main(String[] args) {
+        NtroClientFx.launch(args);
+    }
 
-	@Override
-	public void registerBackend(BackendRegistrar registrar) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void registerFrontend(FrontendRegistrarFx registrar) {
+        registrar.registerFrontend(new FrontalPong());
+    }
 
-	@Override
-	public void registerFrontend(FrontendRegistrarFx registrar) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void registerMessages(MessageRegistrar registrar) {
 
-	@Override
-	public void registerMessages(MessageRegistrar registrar) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void registerModels(ModelRegistrar registrar) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void registerModels(ModelRegistrar registrar) {
+
+    }
+
+    @Override
+    public void registerBackend(BackendRegistrar registrar) {
+
+    }
 
 }
---
+++ pong/src/main/java/pong/frontal/taches/Initialisation.java
@@ -0,0 +1,34 @@
+package pong.frontal.taches;
+
+import ca.ntro.app.tasks.frontend.FrontendTasks;
+import static ca.ntro.app.tasks.frontend.FrontendTasks.*;
+import ca.ntro.app.services.Window;
+
+public class Initialisation {
+
+	public static void creerTaches(FrontendTasks tasks) {
+
+		tasks.taskGroup("Initialisation")
+
+				.contains(subTasks -> {
+
+					afficherFenetre(subTasks);
+
+				});
+	}
+
+	private static void afficherFenetre(FrontendTasks subTasks) {
+
+		subTasks.task("afficherFenetre")
+
+				.waitsFor(window())
+
+				.thenExecutes(inputs -> {
+
+					Window window = inputs.get(window());
+
+					window.show();
+				});
+	}
+
+}
